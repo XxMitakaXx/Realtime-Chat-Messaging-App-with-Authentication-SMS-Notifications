525f26f36acb2762479d5efb8c7e2ed2bb62da54
XxMitakaXx <mitakavarbanov05@gmail.com>
2023-01-30 01:47:52 +0200

    Push

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import { Cockie } from 'universal-cookie';
+
+import { ChannelContainer,  ChannelListContainer} from './components';
+
+const apiKey = '4xzwsvybe5f5';
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+	<div className="app_wrapper">
+		<Chat client={client} theme='team light'>
+			<ChannelListContainer />
+			<ChannelContainer />
+		</Chat>
+	</div>
+}
+
+export default App;
